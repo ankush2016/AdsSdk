922577e6a9a9066353125d704aa31144b563c5cd
Ankush Kapoor <ankush.kapoor@airtel.com>
2021-11-15 23:22:13 +0530

    1.2

--- CommonAdsSdk/src/main/java/com/libs/commonadssdk/AdmobAd.kt
+++ CommonAdsSdk/src/main/java/com/libs/commonadssdk/AdmobAd.kt
@@ -5,8 +5,13 @@ import android.widget.LinearLayout
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class AdmobAd(private val context: Context) {
+    init {
+        MobileAds.initialize(context) {}
+    }
+
     fun inflateBannerAd(bannerAdId: String, adViewLayout: LinearLayout) {
         val adView = AdView(context).apply {
             adSize = AdSize.SMART_BANNER
