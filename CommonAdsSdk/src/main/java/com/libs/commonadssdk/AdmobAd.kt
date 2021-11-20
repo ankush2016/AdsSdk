7d3fd141b84f404d4080d2f95e489059bff8dac3
Ankush Kapoor <ankush.kapoor@airtel.com>
2021-11-20 18:25:47 +0530

    ads

--- CommonAdsSdk/src/main/java/com/libs/commonadssdk/AdmobAd.kt
+++ CommonAdsSdk/src/main/java/com/libs/commonadssdk/AdmobAd.kt
@@ -10,10 +10,9 @@ import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 class AdmobAd(private val context: Context, private val isDebug: Boolean) {
     init {
         MobileAds.initialize(context) {}
-        RequestConfiguration.Builder().setTestDeviceIds(listOf("82A12B078FA435C8680FF1D519559BD7"))
     }
 
-    private val adRequest = AdRequest.Builder().build()
+    //private val adRequest = AdRequest.Builder().build()
     private var mInterstitialAd: InterstitialAd? = null
 
 
@@ -22,11 +21,13 @@ class AdmobAd(private val context: Context, private val isDebug: Boolean) {
             adSize = AdSize.SMART_BANNER
             adUnitId = if (isDebug) "ca-app-pub-3940256099942544/6300978111" else bannerAdId
         }
+        val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
         adViewLayout.addView(adView)
     }
 
     fun loadInterstitialAd(interstitialAdId: String) {
+        val adRequest = AdRequest.Builder().build()
         val adUnitId = if (isDebug) "ca-app-pub-3940256099942544/1033173712" else interstitialAdId
         InterstitialAd.load(context, adUnitId, adRequest, object : InterstitialAdLoadCallback() {
             override fun onAdFailedToLoad(adError: LoadAdError) {
