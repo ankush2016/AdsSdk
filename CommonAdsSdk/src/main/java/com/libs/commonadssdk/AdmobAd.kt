99e04721f36bf3c89169fdf1ccec3e0c177243c9
Ankush Kapoor <ankush.kapoor@airtel.com>
2021-11-20 15:33:06 +0530

    ads sdk

--- CommonAdsSdk/src/main/java/com/libs/commonadssdk/AdmobAd.kt
+++ CommonAdsSdk/src/main/java/com/libs/commonadssdk/AdmobAd.kt
@@ -1,24 +1,63 @@
 package com.libs.commonadssdk
 
+import android.app.Activity
 import android.content.Context
 import android.widget.LinearLayout
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import kotlin.math.min
 
 class AdmobAd(private val context: Context, private val isDebug: Boolean) {
     init {
         MobileAds.initialize(context) {}
     }
 
+    private val adRequest = AdRequest.Builder().build()
+    private var mInterstitialAd: InterstitialAd? = null
+
+
     fun inflateBannerAd(bannerAdId: String, adViewLayout: LinearLayout) {
         val adView = AdView(context).apply {
             adSize = AdSize.SMART_BANNER
             adUnitId = if (isDebug) "ca-app-pub-3940256099942544/6300978111" else bannerAdId
         }
-        val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
         adViewLayout.addView(adView)
+    }
+
+    fun loadInterstitialAd(interstitialAdId: String) {
+        val adUnitId = if (isDebug) "ca-app-pub-3940256099942544/1033173712" else interstitialAdId
+        InterstitialAd.load(context, adUnitId, adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                mInterstitialAd = interstitialAd
+            }
+        })
+    }
+
+    fun showInterstitialAd(interstitialAdId: String, callback: () -> Unit) {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(context as Activity)
+        } else {
+            loadInterstitialAd(interstitialAdId)
+            callback()
+        }
+
+        mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+            override fun onAdDismissedFullScreenContent() {
+                callback()
+            }
+
+            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+            }
+
+            override fun onAdShowedFullScreenContent() {
+                mInterstitialAd = null
+            }
+        }
     }
 }
--- app/src/main/java/com/libs/adssdk/MainActivity.kt
+++ app/src/main/java/com/libs/adssdk/MainActivity.kt
@@ -2,10 +2,34 @@ package com.libs.adssdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.libs.adssdk.databinding.ActivityMainBinding
+import com.libs.commonadssdk.AdmobAd
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var admobAds: AdmobAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        admobAds = AdmobAd(this, BuildConfig.DEBUG)
+        admobAds.inflateBannerAd("", binding.adViewLayout)
+
+        binding.btnShowInterstitial.setOnClickListener {
+            admobAds.showInterstitialAd("") {
+                Toast.makeText(this, "Ad Dismissed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (::admobAds.isInitialized) {
+            admobAds.loadInterstitialAd("")
+        }
     }
 }
