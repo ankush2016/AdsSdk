80b9e112c4d8470f5eee145c9c7385d18fb7a7ae
Ankush Kapoor <ankush.kapoor@airtel.com>
2022-01-15 14:30:23 +0530

    admob

--- app/src/main/java/com/libs/adssdk/MainActivity.kt
+++ app/src/main/java/com/libs/adssdk/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        admobAds = AdmobAd(this, BuildConfig.DEBUG)
+        admobAds = AdmobAd(this, BuildConfig.DEBUG, null)
         admobAds.inflateBannerAd("", binding.adViewLayout)
 
         binding.btnShowInterstitial.setOnClickListener {
