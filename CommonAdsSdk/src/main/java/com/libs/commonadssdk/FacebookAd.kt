9ef7459133ec20ea1c62d94645ac8e224f217da3
Ankush Kapoor <ankush.kapoor@airtel.com>
2022-02-08 00:19:46 +0530

    facebook ads

---
+++ CommonAdsSdk/src/main/java/com/libs/commonadssdk/FacebookAd.kt
@@ -0,0 +1,108 @@
+package com.libs.commonadssdk
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.Log
+import com.facebook.ads.*
+
+class FacebookAd(private val context: Context, private val installer: String?) {//: AudienceNetworkAds.InitListener {
+
+    private val TAG = "FacebookAd"
+    private lateinit var interstitialAd: InterstitialAd
+
+    init {
+        AudienceNetworkAds.initialize(context)
+        if (BuildConfig.DEBUG) {
+            AdSettings.turnOnSDKDebugger(context)
+        }
+        AudienceNetworkAds
+            .buildInitSettings(context)
+            //.withInitListener(this)
+            .initialize()
+    }
+
+    fun setupAndShowInterstitialAd(placementId: String, showInterstitial: Boolean, onAdDismissed: (() -> Unit)?) {
+        var localPlacementId = placementId
+        if (BuildConfig.DEBUG) {
+            localPlacementId = "VID_HD_16_9_15S_APP_INSTALL#YOUR_PLACEMENT_ID"
+        } else {
+            if (isAppDownloadFromPlayStore()) {
+                return
+            }
+        }
+        if (!::interstitialAd.isInitialized) {
+            interstitialAd = InterstitialAd(context, localPlacementId)
+        }
+
+        val interstitialAdListener = object : InterstitialAdListener {
+            override fun onError(ad: Ad?, adError: AdError?) {
+                if (BuildConfig.DEBUG) {
+                    Log.e(TAG, "Interstitial ad failed to load: ${adError?.errorMessage}")
+                }
+            }
+
+            override fun onAdLoaded(ad: Ad?) {
+                if (BuildConfig.DEBUG) {
+                    Log.e(TAG, "Interstitial ad is loaded and ready to be displayed!")
+                }
+            }
+
+            override fun onAdClicked(ad: Ad?) {
+                if (BuildConfig.DEBUG) {
+                    Log.e(TAG, "Interstitial ad clicked!");
+                }
+            }
+
+            override fun onLoggingImpression(ad: Ad?) {
+                if (BuildConfig.DEBUG) {
+                    Log.e(TAG, "Interstitial ad impression logged!")
+                }
+            }
+
+            override fun onInterstitialDisplayed(ad: Ad?) {
+                if (BuildConfig.DEBUG) {
+                    Log.e(TAG, "Interstitial ad displayed.")
+                }
+            }
+
+            override fun onInterstitialDismissed(ad: Ad?) {
+                if (BuildConfig.DEBUG) {
+                    Log.e(TAG, "Interstitial ad dismissed.")
+                }
+                onAdDismissed?.invoke()
+                interstitialAd.loadAd()
+            }
+        }
+
+        interstitialAd.loadAd(
+            interstitialAd
+                .buildLoadAdConfig()
+                .withAdListener(interstitialAdListener)
+                .build()
+        )
+        if (showInterstitial) {
+            if (::interstitialAd.isInitialized && interstitialAd.isAdLoaded) {
+                interstitialAd.show()
+            } else {
+                onAdDismissed?.invoke()
+            }
+        }
+    }
+
+    fun destroyAds() {
+        if (::interstitialAd.isInitialized) {
+            interstitialAd.destroy()
+        }
+    }
+
+    private fun isAppDownloadFromPlayStore(): Boolean {
+        val validInstallers: List<String> = ArrayList(listOf("com.android.vending", "com.google.android.feedback"))
+        return !TextUtils.isEmpty(installer) && validInstallers.contains(installer)
+    }
+
+    /*override fun onInitialized(result: AudienceNetworkAds.InitResult?) {
+        if (BuildConfig.DEBUG) {
+            Log.e(TAG, "onInitialized called -> ${result?.message}")
+        }
+    }*/
+}
--- app/src/main/java/com/libs/adssdk/MainActivity.kt
+++ app/src/main/java/com/libs/adssdk/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.libs.adssdk
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.libs.adssdk.databinding.ActivityMainBinding
 import com.libs.commonadssdk.AdmobAd
+import com.libs.commonadssdk.FacebookAd
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var admobAds: AdmobAd
+    private lateinit var facebookAd: FacebookAd
+
+    private var FB_INTERSTITIAL_PLACEMENT_ID = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,17 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Ad Dismissed", Toast.LENGTH_SHORT).show()
             }
         }
+
+        facebookAds()
+    }
+
+    private fun facebookAds() {
+        facebookAd = FacebookAd(this, getInstallerPackageName(this))
+        binding.btnFbInterstitial.setOnClickListener {
+            facebookAd.setupAndShowInterstitialAd(FB_INTERSTITIAL_PLACEMENT_ID, true) {
+                Toast.makeText(this, "Ad Dismissed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onResume() {
@@ -31,5 +48,22 @@ class MainActivity : AppCompatActivity() {
         if (::admobAds.isInitialized) {
             admobAds.loadInterstitialAd("")
         }
+        if (::facebookAd.isInitialized) {
+            facebookAd.setupAndShowInterstitialAd(FB_INTERSTITIAL_PLACEMENT_ID, false, null)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::facebookAd.isInitialized) {
+            facebookAd.destroyAds()
+        }
+    }
+
+    fun getInstallerPackageName(context: Context): String? {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return context.packageManager.getInstallSourceInfo(BuildConfig.APPLICATION_ID).initiatingPackageName
+        }
+        return context.packageManager.getInstallerPackageName(BuildConfig.APPLICATION_ID)
     }
 }
